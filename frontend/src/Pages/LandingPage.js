4a1e1b8ae8ef5e5dcf831196967577a6839df0d7
xingyu711 <61847270+xingyu711@users.noreply.github.com>
2021-04-01 23:01:13 -0700

    adding fancy background for landing page

--- frontend/src/Pages/LandingPage.js
+++ frontend/src/Pages/LandingPage.js
@@ -1,11 +1,67 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../Images/kitty-friends-logo.svg';
+import Particles from 'react-particles-js';
 
+const particlesOption = {
+  particles: {
+    number: {
+      value: 160,
+      density: {
+        enable: false,
+      },
+    },
+    color: {
+      value: '#CF929E',
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        speed: 4,
+        size_min: 0.3,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      random: true,
+      speed: 1,
+      direction: 'top',
+      out_mode: 'out',
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'bubble',
+      },
+      onclick: {
+        enable: true,
+        mode: 'repulse',
+      },
+    },
+    modes: {
+      bubble: {
+        distance: 250,
+        duration: 2,
+        size: 0,
+        opacity: 0,
+      },
+      repulse: {
+        distance: 400,
+        duration: 4,
+      },
+    },
+  },
+};
 export default function LandingPage() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Particles className="particles" params={particlesOption} />
+      <nav className="navbar navbar-expand-lg navbar-light bg-white">
         <div className="container-fluid">
           <button
             className="navbar-toggler"
