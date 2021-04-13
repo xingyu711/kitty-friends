07ea6e8ceb5837bbf89827bab7eb299bc2bcff25
xingyu711 <61847270+xingyu711@users.noreply.github.com>
2021-04-13 01:53:09 -0700

    attemp to deploy

--- app.js
+++ app.js
@@ -16,10 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
-});
-
 app.use(
   session({
     secret: 'secret for kitty friends web app',
@@ -33,5 +29,9 @@ app.use(
 );
 
 app.use('/', indexRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('frontend', 'build', 'index.html'));
+});
 
 module.exports = app;
